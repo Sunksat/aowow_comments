56b5cbfcb9c8e606d4eae0f0e5d8afacf991dc68
Sunksat <sunksat@hotmail.com>
2018-07-14 19:19:48 +0200

    DB creation for Allakhazam support

---
+++ aowow_allakhazam_comments/aowow_comments_alkz.sql
@@ -0,0 +1,19 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `aowow_comments`
+-- ----------------------------
+DROP TABLE IF EXISTS `aowow_comments_alkz`;
+CREATE TABLE `aowow_comments_alkz` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Comment ID',
+  `type` int(10) unsigned NOT NULL COMMENT 'Type of Page',
+  `typeid` int(10) unsigned NOT NULL COMMENT 'ID Of Page',
+  `userid` bigint(20) unsigned NOT NULL COMMENT 'User ID',
+  `post_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Comment timestap',
+  `commentbody` text COMMENT 'Comment text',
+  `replyto` bigint(20) unsigned DEFAULT NULL COMMENT 'Reply To, comment ID',
+  `edit_userid` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT 'Last Edit User ID',
+  `edit_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Last Edit Time',
+  `edit_count` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Count Of Edits',
+  UNIQUE KEY `id` (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2509516 DEFAULT CHARSET=utf8 COMMENT='udwbase Comments Table';
---
+++ aowow_allakhazam_comments/aowow_comments_rates_alkz.sql
@@ -0,0 +1,15 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `_aowow_comments_rates`
+-- ----------------------------
+DROP TABLE IF EXISTS `aowow_comments_rates_alkz`;
+CREATE TABLE `aowow_comments_rates_alkz` (
+  `commentid` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT 'Comment ID',
+  `userid` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT 'User ID',
+  `rate` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'Rating Set'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='udwbase Comments Rates Table';
+
+-- ----------------------------
+-- Records of _aowow_comments_rates
+-- ----------------------------
